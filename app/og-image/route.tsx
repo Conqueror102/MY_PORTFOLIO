35a0fecb743e03e6049e1c982d65a6806c1cfbcd
victor conqueror <victorvector608@gmail.com>
2025-06-28 17:30:45 +0100

    og image size reduction

--- app/og-image/route.tsx
+++ app/og-image/route.tsx
@@ -14,13 +14,11 @@ export async function GET() {
     return new ImageResponse(
       <div
         style={{
-          background: "white",
           width: "100%",
           height: "100%",
           display: "flex",
           alignItems: "center",
           justifyContent: "center",
-          padding: "40px",
         }}
       >
         {/* Main Logo */}
@@ -28,11 +26,10 @@ export async function GET() {
           src={dataUrl}
           alt="Victor Onyemaechi Logo"
           style={{ 
-            width: 400, 
-            height: 400, 
+            width: 600, 
+            height: 600, 
             borderRadius: "20px", 
             objectFit: "cover", 
-            boxShadow: "0 8px 40px rgba(0, 0, 0, 0.1)" 
           }}
         />
       </div>,
@@ -47,13 +44,11 @@ export async function GET() {
     return new ImageResponse(
       <div
         style={{
-          background: "white",
           width: "100%",
           height: "100%",
           display: "flex",
           alignItems: "center",
           justifyContent: "center",
-          padding: "40px",
         }}
       >
         <div style={{ 
